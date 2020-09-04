8529e0a3862ec80e84ef26faa7718c41aca4ed26
一二三 <1797753943@qq.com>
2020-09-04 18:54:17 +0800

    66666

---
+++ src/org/jd11.java
@@ -0,0 +1,13 @@
+package org;
+
+public class jd11 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("66666");
+	}
+
+}
